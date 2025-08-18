3276d6b65a480b9552d9b4bd275215c7a58159c1
Joshua Killough <joshuakillough@Joshuas-MacBook-Pro.local>
2025-08-17 22:11:19 -0700

    Update server.js port for clarity

--- server.js
+++ server.js
@@ -273,6 +273,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on port ${process.env.PORT || 3000}`);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
